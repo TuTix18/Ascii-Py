3a62ae0e9db7c02a18577b6794b4b1ecd952a0e5
TuTix18 <58310601+TuTix18@users.noreply.github.com>
2019-12-02 22:14:35 +0000

    Update Ascii-Py.py

--- Ascii-Py.py
+++ Ascii-Py.py
@@ -1,6 +1,5 @@
 while True:
     import pyfiglet # pyfiglet lib
-    from os import system
 
     ascii_banner = pyfiglet.figlet_format("Ascii-Py")
     print(ascii_banner)
@@ -9,4 +8,3 @@ while True:
     ascii_result = inp
     if ascii_result.isalpha:
         print(pyfiglet.figlet_format(ascii_result))
-    system("pause")
